fb0b76da0cbd8fb987c91838360400aab812bcff
KimSoEun <thdmds7947@naver.com>
2022-04-12 15:56:11 +0900

    feat: 문법 틀린 것 수정

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 item_api = 'item_api'
 item_id = 'item_id'
 
-@app.get('/${item_api}/')
+@app.get('/{item_api}/')
 async def getItemList():
     return [
         { 
@@ -30,7 +30,7 @@ async def getItemList():
         }
     ]
 
-@app.get('/${item_api}/${item_id}')
+@app.get('/{item_api}/{item_id}')
 async def getItemDetails():
     return {
         "id": 50, 
@@ -44,32 +44,61 @@ async def getItemDetails():
 """ 알람 API입니다. """
 alarm_api = 'alarm_api'
 
-@app.post("/${alarm_api}/")
+@app.post("/{alarm_api}/")
 async def setAlarm():
     return {}
 
-@app.get("/${alarm_api}/")
+@app.get("/{alarm_api}/")
 async def getAlarmList():
     return []
 
-@app.delete("${alarm_api}/")
+@app.delete("{alarm_api}/")
 async def deleteAlarm():
     return {}
 
 """ 사용자 API입니다. """
 user_api = "user_api"
 
-@app.get("${user_api}/")
+@app.get("{user_api}/")
 async def getUserList():
     return [ 
-    {
-        "id": 10, 
-        "name": "박규경",
-        "access_token": "4a49145e-45a8-4845-af45-1af2a59bb044"
-    },
-    {
-        "id": 11, 
-        "name": "박규",
-        "access_token": "4a49145e-45a8-4845-af45-1af2a59bb048"
-    }
-  ] 
+        {
+            "id": 10, 
+            "name": "박규경",
+            "access_token": "4a49145e-45a8-4845-af45-1af2a59bb044"
+        },
+        {
+            "id": 11, 
+            "name": "박규",
+            "access_token": "4a49145e-45a8-4845-af45-1af2a59bb048"
+        }
+    ]
+
+@app.get("{user_api}/visit")
+async def getVisitedItemList():
+    return [
+        {
+        "id": 50, 
+        "name": "[역대최다-13종]17WINTER 레그미인",
+        "broadcaster": "cjmall",
+        "category": "패션·잡화",
+        "price": 78900,
+        "is_alarm_set": True
+        },
+        {
+        "id": 49, 
+        "name": "[역대최다-12종]17WINTER 레그미인",
+        "broadcaster": "cjmall",
+        "category": "패션·잡화",
+        "price": 78900,
+        "is_alarm_set": False
+        },
+        {
+        "id": 48, 
+        "name": "[역대최다-11종]17WINTER 레그미인",
+        "broadcaster": "cjmall",
+        "category": "패션·잡화",
+        "price": 78900,
+        "is_alarm_set": False
+        }
+    ]
